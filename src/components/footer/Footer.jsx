47e53c9397d7a30426aab876254fe887a8a13e98
ktodorov12 <k_todorov12@abv.bg>
2024-07-20 20:57:30 +0300

    Remove main-index file becouse it was separated into different components

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="footer-bottom">
+        <div className="container">
+          <p className="copyright">
+            Copyright &copy; <a href="/">Shop Smart</a> all rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
