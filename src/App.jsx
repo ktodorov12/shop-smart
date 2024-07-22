b0a18139a2a3e476dd8587752c7c0a0ecd8f6137
ktodorov12 <k_todorov12@abv.bg>
2024-07-22 19:36:42 +0300

    Add reac-roter-dom as well as Routes to different pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import Header from "./components/header/Header";
 import Banner from "./components/banner/Banner";
 import ProductCategoryShowcase from "./components/product-showcase/ProductCategoryShowcase";
 import Footer from "./components/footer/Footer";
-import Auth from "./components/auth/Auth";
+import { Routes, Route, Link } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import CrateEdit from "./components/create-edit/CreateEdit";
+import ProductDetails from "./components/product-details/ProductDetails";
+import Profile from "./components/profile/Profile";
 
 function App() {
   return (
@@ -10,11 +15,23 @@ function App() {
       <Header />
 
       <main>
-        <Banner />
-        <ProductCategoryShowcase />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Banner />
+                <ProductCategoryShowcase />
+              </>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/create-product" element={<CrateEdit />} />
+          <Route path="/product/:productId/details" element={<ProductDetails />} />
+          <Route path="/profile/:profileId" element={<Profile />} />
+        </Routes>
       </main>
-
-      {/* <Auth></Auth> */}
 
       <Footer />
     </>
