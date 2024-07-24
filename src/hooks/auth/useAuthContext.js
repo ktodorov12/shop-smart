f731bc8c60e878ed498c880f1aa4f8968e2fb172
ktodorov12 <k_todorov12@abv.bg>
2024-07-24 23:37:40 +0300

    Create context hook for auth that returns the context

---
+++ src/hooks/auth/useAuthContext.js
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+
+export default function useAuthContext() {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw Error("useAuthContext must be inside provider");
+  }
+
+  return context;
+}
