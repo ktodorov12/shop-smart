32eca773fe6f79314c56429fcc0dcd2eeb9a3900
ktodorov12 <k_todorov12@abv.bg>
2024-07-24 23:37:18 +0300

    Create Auth context component which has the context, the provider and a reducer for the state

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useReducer } from "react";
+import { getSessionData } from "../utils/userData";
+
+export const AuthContext = createContext();
+
+export const authReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return { user: action.payload };
+    case "LOGOUT":
+      return { user: null };
+    default:
+      return state;
+  }
+};
+
+export default function AuthProvider({ children }) {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: getSessionData("user"),
+  });
+
+  console.log("AuthContext state: ", state);
+
+  return <AuthContext.Provider value={{ ...state, dispatch }}>{children}</AuthContext.Provider>;
+}
