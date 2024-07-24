5003912cc643af68d296fb57400090af6ce74cba
ktodorov12 <k_todorov12@abv.bg>
2024-07-24 23:24:47 +0300

    Create hook for handling of forms, state in the form and submit of those data to given handler

---
+++ src/hooks/useForm.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export default function useForm(initialValue, callback) {
+  const [data, setData] = useState(initialValue);
+
+  function dataChangeHandler(e) {
+    const nameValue = e.target.name;
+    //TODO add value check here
+    setData((oldState) => ({ ...oldState, [nameValue]: e.target.value }));
+  }
+
+  async function submitHandler(e) {
+    e.preventDefault();
+    const formData = Object.entries({ ...data });
+    const entries = formData.map(([k, v]) => [k, v.trim()]);
+    callback(Object.fromEntries(entries));
+  }
+
+  return { data, dataChangeHandler, submitHandler };
+}
