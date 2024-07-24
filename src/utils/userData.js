65b6d1af6c50bf5f2c57d513ee4af78631c09dcb
ktodorov12 <k_todorov12@abv.bg>
2024-07-24 23:23:38 +0300

    Create utility function with aim of manipulation of localStorage items

---
+++ src/utils/userData.js
@@ -0,0 +1,17 @@
+export function setSessionData(name, data) {
+  localStorage.setItem(name, JSON.stringify(data));
+}
+
+export function getSessionData(name) {
+  const data = JSON.parse(localStorage.getItem(name));
+  return data || "";
+}
+
+export function removeSessionData(name) {
+  localStorage.removeItem(name);
+}
+
+export function getUserToken() {
+  const userData = getSessionData("userData");
+  return userData?.sessionToken;
+}
