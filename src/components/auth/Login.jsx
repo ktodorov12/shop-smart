8f4b3a2fcde5959eef8f703e69e631ded8829664
ktodorov12 <k_todorov12@abv.bg>
2024-07-24 23:40:09 +0300

    Update auth components with corresponding auth hook and useForm hook

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,22 +1,51 @@
-import styles from "./Auth.module.css";
 import { Link } from "react-router-dom";
 
+import styles from "./Auth.module.css";
+import useForm from "../../hooks/useForm";
+import useLogin from "../../hooks/auth/useLogin";
+
+const initialData = {
+  username: "",
+  password: "",
+};
+
 export default function Login() {
+  const { handleLogin, error, isLoading } = useLogin();
+  const {
+    data: userData, 
+    dataChangeHandler, 
+    submitHandler
+  } = useForm(initialData, handleLogin);
+
   return (
     <section className={styles["auth-section"]}>
       <div className={styles["auth-container"]}>
-        <form className={styles["auth-form"]} method="post">
+        <form className={styles["auth-form"]} method="post" onSubmit={submitHandler}>
           <h2>Log in</h2>
           <p>Enter your details to continue</p>
 
           <div className={styles["form-group"]}>
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" name="username" required />
+            <input 
+              type="text" 
+              id="username" 
+              name="username" 
+              required 
+              onChange={dataChangeHandler}
+              value={userData.username}
+            />
           </div>
 
           <div className={styles["form-group"]}>
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" required />
+            <input 
+              type="password" 
+              id="password" 
+              name="password" 
+              required
+              onChange={dataChangeHandler}
+              value={userData.password} 
+            />
           </div>
 
           <button type="submit" className={styles["auth-btn"]}>
--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,37 +1,90 @@
-import styles from "./Auth.module.css";
 import { Link } from "react-router-dom";
 
+import styles from "./Auth.module.css";
+import useForm from "../../hooks/useForm";
+import useSignup from "../../hooks/auth/useSignup";
+
+const initialData = {
+   fullName: "",
+  email: "",
+  username: "",
+  password: "",
+  rePass: "",
+};
+
 export default function Register() {
+  const { signup, error, isLoading } = useSignup();
+  const {
+    data: userData,
+    dataChangeHandler,
+    submitHandler
+  } = useForm(initialData, signup);
+
   return (
     <section className={styles["auth-section"]}>
       <div className={styles["auth-container"]}>
-        <form className={styles["auth-form"]} method="post">
+        <form className={styles["auth-form"]} method="post" onSubmit={submitHandler}>
           <h2>Sign up</h2>
           <p>Enter your details to continue</p>
 
           <div className={styles["form-group"]}>
             <label htmlFor="fullname">Full name</label>
-            <input type="text" id="fullname" name="fullname" required />
+            <input 
+              type="text" 
+              id="fullname" 
+              name="fullName" 
+              required 
+              onChange={dataChangeHandler}
+              value={userData.fullName}
+            />
           </div>
 
           <div className={styles["form-group"]}>
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" required />
+            <input 
+              type="text" 
+              id="email" 
+              name="email" 
+              required 
+              onChange={dataChangeHandler}
+              value={userData.email}
+            />
           </div>
 
           <div className={styles["form-group"]}>
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" name="username" required />
+            <input 
+              type="text" 
+              id="username" 
+              name="username" 
+              required 
+              onChange={dataChangeHandler}
+              value={userData.username}
+            />
           </div>
 
           <div className={styles["form-group"]}>
             <label htmlFor="password">Create your password</label>
-            <input type="password" id="password" name="password" required />
+            <input 
+              type="password" 
+              id="password" 
+              name="password" 
+              required 
+              onChange={dataChangeHandler}
+              value={userData.password}
+            />
           </div>
 
           <div className={styles["form-group"]}>
             <label htmlFor="confirm-password">Confirm password</label>
-            <input type="password" id="confirm-password" name="confirm-password" required />
+            <input 
+              type="password" 
+              id="confirm-password" 
+              name="rePass" 
+              required 
+              onChange={dataChangeHandler}
+              value={userData.rePass}
+            />
           </div>
 
           <button type="submit" className={styles["auth-btn"]}>
