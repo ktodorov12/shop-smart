2b0e27493bf2241ab327b96c95b3160dfed9f2d6
ktodorov12 <k_todorov12@abv.bg>
2024-07-29 18:42:40 +0300

    Add new handler functions for changing nested data and adding new item in nested array in order to maintain single source of truth

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -4,19 +4,37 @@ export default function useForm(initialValue, callback) {
   const [data, setData] = useState(initialValue);
 
   function dataChangeHandler(e) {
-    const nameValue = e.target.name;
+    const { name, value } = e.target;
     //TODO add value check here
-    setData((oldState) => ({ ...oldState, [nameValue]: e.target.value }));
+    setData((oldState) => ({ ...oldState, [name]: value }));
   }
 
-  async function submitHandler(e) {
+  function nestedDataChangeHandler(e, index, arrayName) {
+    const { name, value } = e.target;
+    const newArray = [...data[arrayName]];
+    newArray[index][name] = value;
+
+    setData((oldState) => ({ ...oldState, [arrayName]: newArray }));
+  }
+
+  function addItemToArray(arrayName, newItem) {
+    const newArray = [...data[arrayName], newItem];
+    setData((oldState) => ({ ...oldState, [arrayName]: newArray }));
+  }
+
+  function submitHandler(e) {
     e.preventDefault();
     const formData = Object.entries({ ...data });
-    const entries = formData.map(([k, v]) => [k, v.trim()]);
+    const entries = formData.map(([k, v]) => {
+      if (typeof v === "object") {
+        return [k, v];
+      }
+      return [k, v.trim()];
+    });
     callback(Object.fromEntries(entries));
 
     setData(initialValue);
   }
 
-  return { data, dataChangeHandler, submitHandler };
+  return { data, dataChangeHandler, submitHandler, nestedDataChangeHandler, addItemToArray };
 }
