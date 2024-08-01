adbd82cd6209377257023e2c9dc4f45056133b96
ktodorov12 <k_todorov12@abv.bg>
2024-08-01 19:58:56 +0300

    Create modal components for notifications and delete

---
+++ client/src/components/modals/notification/Notification.jsx
@@ -0,0 +1,21 @@
+export default function NotificationModal() {
+  return (
+    <div className="notification-toast" data-toast>
+      <button className="toast-close-btn" data-toast-close>
+        <ion-icon name="close-outline"></ion-icon>
+      </button>
+
+      <div className="toast-banner">
+        <img src="./assets/images/products/jewellery-1.jpg" alt="Rose Gold Earrings" width="80" height="70" />
+      </div>
+
+      <div className="toast-detail">
+        <p className="toast-message">Someone in new just bought</p>
+
+        <p className="toast-title">Rose Gold Earrings</p>
+
+        <p className="toast-meta">{/* <time datetime="PT2M">2 Minutes</time> ago */}</p>
+      </div>
+    </div>
+  );
+}
