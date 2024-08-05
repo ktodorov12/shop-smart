aa6635cddcaf408ac5d007360101f300909bf8a9
ktodorov12 <k_todorov12@abv.bg>
2024-08-05 22:44:09 +0300

    fix of infinite fetching bug

--- client/src/hooks/products/useGetDetails.js
+++ client/src/hooks/products/useGetDetails.js
@@ -1,13 +1,34 @@
-import useFetch from "../../hooks/useFetch";
 import { useParams } from "react-router-dom";
 
 import { getProductById } from "../../api/apiProducts";
 import { useAuthContext } from "../../contexts/AuthContext";
+import { useCallback, useEffect, useState } from "react";
 
 export default function useGetDetails() {
   const { productId } = useParams();
-  const { values: product, isLoading, error } = useFetch({}, () => getProductById(productId));
   const { user, isOwner: funcIsOwner } = useAuthContext();
+
+  const [product, setProduct] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const memorizedFetch = useCallback(getProductById, [getProductById]);
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+      setError(null);
+
+      try {
+        const res = await memorizedFetch(productId);
+        setProduct(res);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, [productId, memorizedFetch]);
 
   const isGuest = !!user;
   const isOwner = funcIsOwner(product?._ownerId);
