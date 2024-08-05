c36a96f110419af39f2cc21c7cac2411228cb0fd
ktodorov12 <k_todorov12@abv.bg>
2024-08-05 18:54:51 +0300

    Make checkout togglable

--- client/src/components/checkout/Checkout.jsx
+++ client/src/components/checkout/Checkout.jsx
@@ -1,34 +1,38 @@
-export default function Checkout() {
+export default function Checkout({ onClose }) {
   return (
-    <div className="checkout">
-      <div className="checkout-header">
-        <h2>CART</h2>
-        <button className="close-button">&times;</button>
-      </div>
-      <ul>
-        <li className="checkout-item">
-          <img src="/assets/images/products/jacket-5.jpg" alt="name" className="item-image" />
-          <div className="item-details">
-            <span className="item-name">Jacket</span>
-            <span className="item-total">
-              Total: <b>$100</b>
-            </span>
-            <div className="item-quantity">
-              <span>Quantity:</span>
-              <button>-</button>
-              <span>1</span>
-              <button>+</button>
-            </div>
-            <button className="remove-button">Remove</button>
-          </div>
-        </li>
-      </ul>
-      <div className="order-summary">
-        <div className="summary-item total">
-          <span>Total:</span>
-          <span>$100</span>
+    <div className="modal" data-modal>
+      <div className="modal-close-overlay" data-modal-overlay onClick={onClose}></div>
+
+      <div className="checkout">
+        <div className="checkout-header">
+          <h2>CART</h2>
+          <button className="close-button" onClick={onClose}>&times;</button>
         </div>
-        <button className="checkout-button">Checkout</button>
+        <ul>
+          <li className="checkout-item">
+            <img src="/assets/images/products/jacket-5.jpg" alt="name" className="item-image" />
+            <div className="item-details">
+              <span className="item-name">Jacket</span>
+              <span className="item-total">
+                Total: <b>$100</b>
+              </span>
+              <div className="item-quantity">
+                <span>Quantity:</span>
+                <button>-</button>
+                <span>1</span>
+                <button>+</button>
+              </div>
+              <button className="remove-button">Remove</button>
+            </div>
+          </li>
+        </ul>
+        <div className="order-summary">
+          <div className="summary-item total">
+            <span>Total:</span>
+            <span>$100</span>
+          </div>
+          <button className="checkout-button">Checkout</button>
+        </div>
       </div>
     </div>
   );
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+import Checkout from "../checkout/Checkout";
+
 import useLogout from "../../hooks/auth/useLogout";
+
 import useAuthContext from "../../hooks/auth/useAuthContext";
 import { useLikeContext } from "../../contexts/LikedContext";
 import { useShoppingBagContext } from "../../contexts/ShoppingBagContext";
 
 export default function Header() {
+  const [openBag, setOpenBag] = useState(false);
+
   const { logout } = useLogout();
   const { user } = useAuthContext();
   const { liked } = useLikeContext();
@@ -15,78 +21,85 @@ export default function Header() {
     e.preventDefault();
     logout();
   }
+
+  const handleOpenBag = () => setOpenBag(true);
+  const handleCloseBag = () => setOpenBag(false);
+
   return (
-    <header>
-      <div className="header-main">
-        <div className="container">
-          <Link to="/" className="header-logo">
-            <img src="/assets/images/logo/logo-no-background.png" alt="Shop Smart's logo" width="80" height="60" />
-          </Link>
-
-          <div className="header-search-container">
-            <input type="search" name="search" className="search-field" placeholder="Enter your product name..." />
-
-            <button className="search-btn">
-              <ion-icon name="search-outline"></ion-icon>
-            </button>
-          </div>
-
-          <div className="header-user-actions">
-            <div className="container">
-              <ul className="desktop-menu-category-list">
-                <li className="menu-category">
-                  <Link to="/" className="menu-title">
-                    Home
-                  </Link>
-                </li>
-
-                {user ? (
-                  <>
-                    <li className="menu-category">
-                      <Link to="/add-product" className="menu-title">
-                        Add
-                      </Link>
-                    </li>
-                    <li className="menu-category">
-                      <Link className="menu-title" onClick={handleLogout}>
-                        Logout
-                      </Link>
-                    </li>
-                  </>
-                ) : (
-                  <>
-                    <li className="menu-category">
-                      <Link to="/register" className="menu-title">
-                        Register
-                      </Link>
-                    </li>
-                    <li className="menu-category">
-                      <Link to="/login" className="menu-title">
-                        Login
-                      </Link>
-                    </li>
-                  </>
-                )}
-              </ul>
-            </div>
-            {user && (
-              <Link className="action-btn" to={`/profile/${user._id}`}>
-                <ion-icon name="person-outline"></ion-icon>
-              </Link>
-            )}
-
-            <Link to={"/favourites"} className="action-btn">
-              <ion-icon name="heart-outline"></ion-icon>
-              <span className="count">{liked.length}</span>
+    <>
+      {openBag && <Checkout onClose={handleCloseBag}/>}
+      <header>
+        <div className="header-main">
+          <div className="container">
+            <Link to="/" className="header-logo">
+              <img src="/assets/images/logo/logo-no-background.png" alt="Shop Smart's logo" width="80" height="60" />
             </Link>
 
-            <button className="action-btn">
-              <ion-icon name="bag-handle-outline"></ion-icon>
-              <span className="count">{addedToBag.length}</span>
-            </button>
+            <div className="header-search-container">
+              <input type="search" name="search" className="search-field" placeholder="Enter your product name..." />
+
+              <button className="search-btn">
+                <ion-icon name="search-outline"></ion-icon>
+              </button>
+            </div>
+
+            <div className="header-user-actions">
+              <div className="container">
+                <ul className="desktop-menu-category-list">
+                  <li className="menu-category">
+                    <Link to="/" className="menu-title">
+                      Home
+                    </Link>
+                  </li>
+
+                  {user ? (
+                    <>
+                      <li className="menu-category">
+                        <Link to="/add-product" className="menu-title">
+                          Add
+                        </Link>
+                      </li>
+                      <li className="menu-category">
+                        <Link className="menu-title" onClick={handleLogout}>
+                          Logout
+                        </Link>
+                      </li>
+                    </>
+                  ) : (
+                    <>
+                      <li className="menu-category">
+                        <Link to="/register" className="menu-title">
+                          Register
+                        </Link>
+                      </li>
+                      <li className="menu-category">
+                        <Link to="/login" className="menu-title">
+                          Login
+                        </Link>
+                      </li>
+                    </>
+                  )}
+                </ul>
+              </div>
+              {user && (
+                <Link className="action-btn" to={`/profile/${user._id}`}>
+                  <ion-icon name="person-outline"></ion-icon>
+                </Link>
+              )}
+
+              <Link to={"/favourites"} className="action-btn">
+                <ion-icon name="heart-outline"></ion-icon>
+                <span className="count">{liked.length}</span>
+              </Link>
+
+              <button className="action-btn" onClick={openBag ? handleCloseBag : handleOpenBag}>
+                <ion-icon name="bag-handle-outline"></ion-icon>
+                <span className="count">{addedToBag.length}</span>
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 }
