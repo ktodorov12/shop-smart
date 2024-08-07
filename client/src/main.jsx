686f1cf793a61b868a443f1fbdb5c4c6e4b4d735
ktodorov12 <k_todorov12@abv.bg>
2024-08-07 20:47:41 +0300

    Create ErrorBaundary class component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import ErrorBoundary from "./components/error-boundary/ErrorBoundary.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </React.StrictMode>
   </BrowserRouter>
 );
