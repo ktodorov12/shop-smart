85a4836fc9dd5b6c8a86c1bb695354d9f5505482
ktodorov12 <k_todorov12@abv.bg>
2025-08-26 18:16:06 +0300

    Add testing libraries

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './tests/setup.js',
+  }
+} as UserConfig)
